89f5e54abb86698165d8442a93cb56d46cab6040
SeanH <85364282+SeanHuebl@users.noreply.github.com>
2025-01-13 21:27:07 +0000

    created basic server, health endpoint works

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/seanhuebl/unity-wealth/handlers"
+)
+
+type apiConfig struct {
+	port         string
+	secretString string
+}
+
 func main() {
+	godotenv.Load()
+	var cfg apiConfig
+	cfg.port = fmt.Sprintf(":%v", os.Getenv("PORT"))
+	cfg.secretString = os.Getenv("JWT_SECRET")
+
+	router := gin.Default()
+
+	router.GET("/health", handlers.Health)
+
+	err := router.Run(cfg.port)
+	if err != nil {
+		log.Fatal("error starting server")
+	}
 
 }
